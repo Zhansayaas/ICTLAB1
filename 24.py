fc0987b3f2bc8f1925b74a07bfbdae679d0f4b14
Zhansayaas <65022786+Zhansayaas@users.noreply.github.com>
2020-09-16 22:25:04 +0600

    Add files via upload

---
+++ 24.py
@@ -0,0 +1,9 @@
+days = int(input("Please enter a number of days:"))
+hours = int(input("Please enter a number of hours: "))
+minutes = int(input("Please enter a number of minutes: "))
+seconds = int(input("Please enter a number of seconds: "))
+totalSeconds = seconds
+totalSeconds += minutes * 60
+totalSeconds += hours * 60 * 60
+totalSeconds += days * 60 * 60 * 241
+print("The total number of seconds is {}.".format(totalSeconds))
